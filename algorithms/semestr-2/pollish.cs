65446fe4f3dffa757a6ecd8e36d52d65eaa266b8
Vadim-Eryomin <49606493+Vadim-Eryomin@users.noreply.github.com>
2024-02-28 13:21:00 +0600

    Create pollish.cs

---
+++ algorithms/semestr-2/pollish.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace fiteryomin
+{
+    class Programm
+    {
+        static void Main()
+        {
+            string eval = Console.ReadLine(); // "1 2 + 5 * 4 /"
+            Stack numbers = new Stack();
+            foreach (var e in eval.Split(' '))
+            {
+                if (e == "+" || e == "*" || e == "-" || e == "/")
+                {
+                    if (numbers.Count < 2)
+                    {
+                        Console.WriteLine("Недостаточно операндов!");
+                        Console.ReadKey();
+                        return;
+                    }
+                    double a = (double)numbers.Pop(), b = (double)numbers.Pop();
+                    numbers.Push(Operate(b, a, e));
+                }
+                else
+                {
+                    try
+                    {
+                        numbers.Push(double.Parse(e));
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Ошибка чтения данных");
+                    }
+                }
+            }
+
+            Console.WriteLine(numbers.Pop());
+            Console.ReadKey();
+        }
+
+        static double Operate(double a, double b, string operation)
+        {
+            switch (operation)
+            {
+                case "+":
+                    return a + b;
+                case "-":
+                    return a - b;
+                case "*":
+                    return a * b;
+                case "/":
+                    return a / b;
+            }
+            return 0;
+        }
+    }
+}
