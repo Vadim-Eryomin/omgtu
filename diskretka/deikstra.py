35ab16fe348269aff8d9932d96b031796d0dbf33
Vadim-Eryomin <49606493+Vadim-Eryomin@users.noreply.github.com>
2024-03-07 11:10:54 +0600

    Create deikstra.py

---
+++ diskretka/deikstra.py
@@ -0,0 +1,67 @@
+gr = [(1, 2, 41), (1, 3, 80), (1, 4, 23), (1, 5, 32), (2, 1, 41), (2, 3, 45), (2, 4, 12), (2, 5, 37), (3, 1, 80),
+      (3, 2, 45), (3, 4, 50), (3, 5, 64), (4, 1, 23), (4, 2, 12), (4, 3, 50), (4, 5, 67), (5, 1, 32), (5, 2, 37),
+      (5, 3, 64), (5, 4, 67)]
+
+inf = 10 ** 10
+min_length = [inf, inf, inf, inf, inf]
+min_targets = [inf, inf, inf, inf, inf]
+
+start, end = 4, 3
+current = start - 1
+min_targets[current], min_length[current] = 0, 0
+
+for i in range(len(min_length) - 1):
+    for j in range(len(min_length)):
+        if current+1 == j+1:
+            continue  # петли не рассматриваем
+
+        weight = [x for x in gr if x[0] == current+1 and x[1] == j+1]
+        if weight:
+            weight = weight[0][2]
+            min_length[j] = min(min_length[j], min_targets[current] + weight)
+
+    # ищем следующую вершину
+    # минимальная длина для следующей вершины
+    min_tar = min([min_length[i] for i, x in enumerate(min_targets) if x == inf])
+    # вершина, для которой это выполняется
+    current = [i for i, x in enumerate(min_length) if x == min_tar][0]
+    # для нее путь уже кратчайший
+    min_targets[current] = min_length[current]
+    # если дошли до нужной, то заканчиваем
+    if current == end:
+        break
+
+path_length = min_targets[end-1]
+path = [end]
+if path_length == inf:
+    print("Пути нет")
+    exit()
+
+print(path_length)
+print(min_targets)
+for i in range(len(min_length)):
+    for j in range(len(min_length)):
+        if current == j:
+            continue
+        print(j, path[-1])
+        print(j, [x for x in gr if x[0] == j+1 and x[1] == path[-1]])
+        weight = [x for x in gr if x[0] == j+1 and x[1] == path[-1]][0][2]
+
+        if min_targets[j] + weight == path_length:
+            path_length -= weight
+            path.append(j)
+            break
+
+    if current == start:
+        break
+print(list(reversed(path)))
+
+
+
+
+
+
+
+
+
+
