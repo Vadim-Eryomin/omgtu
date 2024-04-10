6e0dcf0fc971de0c3f481cdaab1ac5f844aa83d3
Vadim-Eryomin <49606493+Vadim-Eryomin@users.noreply.github.com>
2024-04-10 11:45:44 +0600

    Update generic_math.cs

--- algorithms/semestr-2/generic_math.cs
+++ algorithms/semestr-2/generic_math.cs
@@ -11,7 +11,13 @@ namespace fiteryomin
 
         static void Main()
         {
-            Calc<int> calculator = new Calc<int>();
+            Calc<int> calculatorInt = new Calc<int>();
+            Console.WriteLine(calculatorInt.Sum(5, 80));
+            Console.WriteLine(calculatorInt.Diff(90, 6));
+
+            Calc<double> calculatorDouble = new Calc<double>();
+            Console.WriteLine(calculatorDouble.Mul(5, 6));
+            Console.WriteLine(calculatorDouble.Div(90, 6));
         }
 
     }
@@ -23,7 +29,30 @@ namespace fiteryomin
 
         public T Sum(T a, T b)
         {
-            return a + b;
+            dynamic c = a;
+            dynamic d = b;
+            return c + d;
+        }
+
+        public T Diff(T a, T b)
+        {
+            dynamic c = a;
+            dynamic d = b;
+            return c - d;
+        }
+
+        public T Mul(T a, T b)
+        {
+            dynamic c = a;
+            dynamic d = b;
+            return c * d;
+        }
+
+        public T Div(T a, T b)
+        {
+            dynamic c = a;
+            dynamic d = b;
+            return c / d;
         }
 
 
