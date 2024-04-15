18f4e46e3fe9d4cc61b1bff819c40938b8a94380
Vadim-Eryomin <49606493+Vadim-Eryomin@users.noreply.github.com>
2024-04-15 08:49:14 +0600

    Create min_podstroka_s_a.cs

---
+++ algorithms/semestr-2/min_podstroka_s_a.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Linq;
+
+namespace fiteryomin
+{
+
+    class Program
+    {
+        static string nAs(int n)
+        {
+            StringBuilder As = new StringBuilder();
+            for (int i = 0; i < n; i++)
+                As.Append("a");
+            return As.ToString();
+        }
+
+        static void Main() 
+        {
+            string[] data = File.ReadAllLines("a.txt").Where(s => s.Contains("a")).ToArray();
+            if (data.Length == 0)
+            {
+                Console.WriteLine("Нет букв а");
+                return;
+            }
+                
+
+            int[] countAs = new int[data.Length];
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                string line = data[i];
+                for (int j = 0; j < line.Length; j++)
+                {
+                    if (line.Contains(nAs(j))) countAs[i] = j;
+                    else break;
+                }
+            }
+
+
+            int min = countAs.Min();
+            if (min == 0)
+                Console.WriteLine("Нет букв а");
+            else
+            {
+                int index = Array.IndexOf(countAs, min);
+                Console.WriteLine(data[index]);
+            }
+        }
+
+    }
+
+}
+
