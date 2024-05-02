d5f07d09c6bacfa170d9379a8be3d7d56454dcc5
Vadim-Eryomin <49606493+Vadim-Eryomin@users.noreply.github.com>
2024-05-02 10:42:57 +0600

    Update linq-3.cs

--- algorithms/semestr-2/linq-3.cs
+++ algorithms/semestr-2/linq-3.cs
@@ -9,22 +9,19 @@ namespace ConsoleApplication1
     class Program
     {
         
-
         static void Main(string[] args)
         {
             string[] data = new string[] { "12345", "123456789", "1234567", "12345678", "1234567", "123456789", "1234", "12345678" };
-            data
-                .Where(e => e.Length % 2 == 0)
-                .ToList()
-                .ForEach(e => Console.Write(e + " "));
-            Console.WriteLine();
-            
-            data = data.Where((e, index) => index % 2 == 0).ToArray();
+            for (int i = 0; i < 2; i++)
+            {
+                data
+                    .Where(e => e.Length % 2 == 0)
+                    .ToList()
+                    .ForEach(e => Console.Write(e + " "));
+                Console.WriteLine();
 
-            data
-                .Where((e, index) => e.Length % 2 == 0)
-                .ToList()
-                .ForEach(e => Console.Write(e + " "));
+                data = data.Where((e, index) => index % 2 == 0).ToArray();
+            }
         }
     }
 }
