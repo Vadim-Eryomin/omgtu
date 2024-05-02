7711d6a46eaea5a5bbaf0a26b2f1e2f667e762fc
Vadim-Eryomin <49606493+Vadim-Eryomin@users.noreply.github.com>
2024-05-02 10:25:10 +0600

    Create linq-3.cs

---
+++ algorithms/semestr-2/linq-3.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] data = new string[] { "12345", "123456789", "1234567", "12345678", "1234567", "123456789", "1234", "12345678" };
+            data
+                .Where(e => e.Length % 2 == 0)
+                .ToList()
+                .ForEach(e => Console.Write(e + " "));
+
+            Console.WriteLine();
+
+            data
+                .Where((e, index) => index % 2 == 0 && e.Length % 2 == 0)
+                .ToList()
+                .ForEach(e => Console.Write(e + " "));
+        }
+    }
+}
